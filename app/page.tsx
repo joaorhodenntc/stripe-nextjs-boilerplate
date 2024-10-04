8b13a90de751c3841e3486e315d2fc35a7d77c3e
João Rhoden <j.machado02@edu.pucrs.br>
2024-10-04 01:13:35 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import BuyButton from "./components/buy-button";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <BuyButton/>
+    </div>
+  );
+}
---
+++ app/sucesso/page.tsx
@@ -0,0 +1,7 @@
+export default function Sucesso() {
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-center">
+            <h1>Obrigado pela compra!</h1>
+        </div>
+    );
+}
